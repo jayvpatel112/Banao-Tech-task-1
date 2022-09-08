a93ffd96066d669db6632e48289016dc96fe9b2d
Jay <pateljay.india@gmail.com>
2022-09-08 14:09:30 +0530

    task 2 added

--- Banao_tech_task1/settings.py
+++ Banao_tech_task1/settings.py
@@ -75,13 +75,36 @@ WSGI_APPLICATION = 'Banao_tech_task1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'banao_tech_db',
+#         'USER': 'root',
+#         'PASSWORD': 'Jay@123',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'banao_tech_db',
+        'USER': 'postgres',
+        'PASSWORD': 'jay',
+        'HOST': 'localhost'
     }
 }
 
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:jay@localhost/banao_tech_db')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
